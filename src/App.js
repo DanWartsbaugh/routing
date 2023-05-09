adfa7109a264889565cef70bcfc0303008cd42ee
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-05-09 12:51:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes,Route,Link} from "react-router-dom";
+import {useParams} from "react-router";
+
+const Home = (props) => {
+  return (
+    <div>
+        <h1>Welcome home</h1>        
+    </div>
+  );
+}
+
+const Text = (props) => {
+  const {text} = useParams()
+  // isNaN(text)?
+  if(isNaN(text)){
+  return (
+    <h1>
+      The word is: {text}
+    </h1>
+  )}
+  else{
+  return (
+    <h1>
+      The number is: {text}
+    </h1>
+  )}
+}
+
+const TextPlus = (props) => {
+  const {text,color,background} = useParams()
+  // isNaN(text)?
+  if(isNaN(text)){
+  return (
+    <h1 style={{color:color,backgroundColor:background}}>
+      The word is: {text}
+    </h1>
+  )}
+  else{
+  return (
+    <h1 style={{color:color,backgroundColor:background}}>
+      The number is: {text}
+    </h1>
+  )}
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/:text" element={<Text/>} />
+        <Route path="/:text/:color/:background" element={<TextPlus/>} />
+
+      </Routes>
     </div>
   );
 }
